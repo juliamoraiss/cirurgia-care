0a2c87db1548e01640b5d7479bdeb6383ec7db90
Julia <julia.morais@external.stellantis.com>
2025-10-24 20:11:53 -0300

    fix capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.lovable.medsystem',
   appName: 'medsystem',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"]
+    }
+  }
 };
 
 export default config;
